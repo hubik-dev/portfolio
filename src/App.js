30c5769017679e2ee18fac5e2dedb9ac591acf61
Bence Hubik <hubik.bence@outlook.com>
2023-11-23 15:24:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <main>
+      <Header></Header>
+      <h1>Hello world!</h1>
+    </main>
+  );
+}
+
+export default App;
