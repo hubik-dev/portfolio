fde95892b95de99d0b3ac630f0a0678c7e7d4429
Bence Hubik <hubik.bence@outlook.com>
2023-11-25 15:25:29 +0100

    context

--- src/components/Header/MainMenu/Hamburger/Hamburger.js
+++ src/components/Header/MainMenu/Hamburger/Hamburger.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import styles from "./Hamburger.module.scss";
 
 const Hamburger = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+  };
+
+  const hamburgerClasses = isOpen
+    ? `${styles.hamburger} ${styles.open}`
+    : styles.hamburger;
+
   return (
-    <div className={styles.hamburger}>
+    <div className={hamburgerClasses} onClick={handleToggle}>
       <span></span>
       <span></span>
       <span></span>
