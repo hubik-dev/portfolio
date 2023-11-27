e74d805a8493138303f3647aa47be950209cf1e1
Bence Hubik <hubik.bence@outlook.com>
2023-11-27 19:17:37 +0100

    ContentTop start

---
+++ src/components/MainContent/ContentTop/ContentTop.js
@@ -0,0 +1,11 @@
+import ContentTopLeft from "./ContentTopLeft/ContentTopLeft";
+
+const ContentTop = () => {
+  return (
+    <div className="row">
+      <ContentTopLeft />
+    </div>
+  );
+};
+
+export default ContentTop;
---
+++ src/components/MainContent/ContentTop/ContentTopLeft/ContentTopLeft.js
@@ -0,0 +1,9 @@
+const ContentTopLeft = () => {
+  return (
+    <div className="col-8">
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+    </div>
+  );
+};
+
+export default ContentTopLeft;
