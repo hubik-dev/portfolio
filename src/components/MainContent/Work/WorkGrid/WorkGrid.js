9241d51750dd4a4ae2219ed24e26b0f365c2372d
Bence Hubik <hubik.bence@outlook.com>
2023-11-29 14:55:26 +0100

    stuff/ svg stil sucks ass

--- src/components/MainContent/Work/WorkGrid/WorkGrid.js
+++ src/components/MainContent/Work/WorkGrid/WorkGrid.js
@@ -1,7 +1,18 @@
+import WorkGridItem from "./WorkGridItem/WorkGridItem";
+import bg from "../../../../assets/main/work/misc/bg.svg";
+
 const WorkGrid = () => {
   return (
     <div>
-      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3"></div>
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+        <WorkGridItem
+          src={bg}
+          alt="alt"
+          title="title"
+          desc="desc"
+          techStack="techStack"
+        />
+      </div>
     </div>
   );
 };
